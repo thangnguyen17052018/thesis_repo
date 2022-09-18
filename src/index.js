c18d339aa249479e788f802c7bf677540f5e9ee3
vinh <vinhphamdev0406@gmail.com>
2022-09-18 20:47:55 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "redux-zero/react";
+import store from "./redux/configureStore";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/redux/actions.js
@@ -0,0 +1,7 @@
+/* actions.js */
+const actions = (store) => ({
+  increment: (state) => ({ count: state.count + 1 }),
+  decrement: (state) => ({ count: state.count - 1 }),
+});
+
+export default actions;
